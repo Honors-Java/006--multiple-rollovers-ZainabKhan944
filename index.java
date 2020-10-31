ca54c9babeb9bea16e1d47fd6e588267a520d37c
ZainabKhan944 <zyk0517@gmail.com>
2020-10-31 00:46:24 +0000

    Finished the assignment- making a corner turn black if the mouse hovers over it.

--- index.java
+++ index.java
@@ -7,12 +7,27 @@ void draw() {
 	stroke(0);
 
   //start creating the lines
-
+  line(200, 0, 200, 400);
+  line(0, 200, 400, 200);
 
   //Fill a black color
   
 
   //Build your conditional statement 
-	
-
+  if (mouseX > width/2 && mouseY > height/2) {
+  fill(0);
+  rect(width/2, height/2, width/2, height/2);
+  }
+  if (mouseX > width/2 && mouseY < height/2) {
+  fill(0);
+  rect(width/2, height * 0 ,width/2, height/2);
+  }
+  if (mouseX < width/2 && mouseY < height/2) {
+  fill(0);
+  rect(width * 0, height * 0 ,width/2, height/2);
+} 
+if (mouseX < width/2 && mouseY > height/2) {
+  fill(0);
+  rect(width * 0, height/2 ,width/2, height/2);
+}
 }
